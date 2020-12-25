d0fd8eadb95c2dfc0ab9d83fee7624200f63fe95
pepm17 <pablopupo1797@gmail.com>
2020-12-25 00:28:58 -0500

    Add: Provider Interface - implementation CharacterSheet and Hability Repository

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,15 +2,24 @@
 
 namespace App\Providers;
 
+use Src\CharacterSheet\Domain\Contracts\CharacterSheetRepository;
+use Src\CharacterSheet\Infrastructure\EloquentCharacterSheetRepository;
+
+use Src\Hability\Domain\Contracts\HabilityRepository;
+use Src\Hability\Infrastructure\EloquentHabilityRepository;
+
 use Src\User\Domain\Contracts\UserRepository;
 use Src\User\Infrastructure\EloquentUserRepository;
+
 use Src\Shared\Domain\CommandBus;
 use Src\Shared\Domain\Container;
 use Src\Shared\Domain\Inflector;
 use Src\Shared\Infrastructure\InMemoryLaravelCommandBus;
 use Src\Shared\Infrastructure\LaravelContainer;
 use Src\Shared\Infrastructure\NameInflector;
+
 use Illuminate\Support\ServiceProvider;
+use Src\Hability\Application\Find\FindHabilityUseCase;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -32,6 +41,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->app->bind(UserRepository::class, EloquentUserRepository::class);
+        $this->app->bind(CharacterSheetRepository::class, EloquentCharacterSheetRepository::class);
+        $this->app->bind(HabilityRepository::class, EloquentHabilityRepository::class);
 
         $this->app->bind(CommandBus::class, InMemoryLaravelCommandBus::class);
         $this->app->bind(Container::class, LaravelContainer::class);
